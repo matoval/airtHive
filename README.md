a7e7b51cde344e39c82e63658fef377bd21f1d04
matoval <mat24c@gmail.com>
2025-02-20 20:27:52 -0800

    create gtk starter app and call if from main.go

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # airtHive
+
+Requires llama-cli and rpc-server, guide to build these from [llama.cpp docs](https://github.com/ggml-org/llama.cpp/blob/master/examples/rpc/README.md)
--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+/*
+#cgo pkg-config: gtk4
+#include "gtk_gui/app.c"
+*/
+import "C"
+import "fmt"
+
 func main() {
-	
+    fmt.Println("Calling gtk_gui application from Go...")
+    C.start(0, nil)
 }
+
